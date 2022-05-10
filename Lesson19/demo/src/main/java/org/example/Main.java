4df9f40a2ecef7652633096273bf7eb563fdc38d
Dmitriy <nepich@gmail.com>
2022-05-10 12:28:36 +0300

    Проблема в 19 уроке заключалась в том ч, что перед путем к БД нужно указывать драйвер. РЕШЕНО! Работает!

--- Lesson19/demo/src/main/java/org/example/Main.java
+++ Lesson19/demo/src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             Class.forName("org.sqlite.JDBC");
-            Connection con = DriverManager.getConnection("src\\main\\DB\\new_db.db");
+            Connection con = DriverManager.getConnection("jdbc:sqlite:src\\main\\DB\\new_db.db");
             Statement stmt = con.createStatement();
             String query = "CREATE TABLE names (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);";
             stmt.executeUpdate(query);
